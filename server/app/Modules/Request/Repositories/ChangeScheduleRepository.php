2d207837e8453c7cefdc3754e79a61f32c372f82
Meljohn O Ugaddan <ugaddanmeljohn@gmail.com>
2020-11-28 16:10:51 +0800

    BUGFIX - Change Schedule
    - Added condition for checking if workdays exists
    - bind_to and schedule_details added

--- server/app/Modules/Request/Repositories/ChangeScheduleRepository.php
+++ server/app/Modules/Request/Repositories/ChangeScheduleRepository.php
@@ -206,9 +206,12 @@ class ChangeScheduleRepository implements ChangeScheduleRepositoryInterface{
 
             # Iterates the Array fetched from the Drupal Database
             foreach( $drupal_evox_change_schedules_array as $drupal_evox_change_schedule) {
+
                 $user = User::where(['emp_num' => $drupal_evox_change_schedule->employee_number])->first();
 
                 if(!is_null($user )) {
+                    $work_days = explode(",", $drupal_evox_change_schedule->work_days);
+
                     $schedule = new ScheduleRepository();
 
                     $work_days = explode(",", $drupal_evox_change_schedule->work_days);
@@ -218,55 +221,54 @@ class ChangeScheduleRepository implements ChangeScheduleRepositoryInterface{
                         'work_days' => $work_days,
                         'source_type' => 'change_schedule',
                         'schedule_type' => 'customize',
+                        'bind_to' => 'user',
                         'schedule_policies' => array(
                             'allow_night_diff' =>  $drupal_evox_change_schedule->nightdiff,
                             'allow_late' =>  $drupal_evox_change_schedule->late,
                             'allow_undertime' => $drupal_evox_change_schedule->undertime
-                        )                        
+                        ),
+                        'schedule_details' => array()                        
                     );
 
                     foreach ($work_days as $key => $value) {
                         if(is_valid($value)){
-                            $data['schedule_details'][$value]['start_time'] = $drupal_evox_change_schedule->{$value . "_on_duty" };
-                            $data['schedule_details'][$value]['end_time'] = $drupal_evox_change_schedule->{$value . "_off_duty" };
-                            $data['schedule_details'][$value]['break_time'] = $drupal_evox_change_schedule->{$value . "_breaktime" };
-                            $data['schedule_details'][$value]['start_flexy_time'] =  $drupal_evox_change_schedule->{$value . "_flexy_start" };
-                            $data['schedule_details'][$value]['end_flexy_time'] = $drupal_evox_change_schedule->{$value . "_flexy_end" };
+                        $data['schedule_details'][$value]['start_time'] = $drupal_evox_change_schedule->{$value . "_on_duty" };
+                        $data['schedule_details'][$value]['end_time'] = $drupal_evox_change_schedule->{$value . "_off_duty" };
+                        $data['schedule_details'][$value]['break_time'] = $drupal_evox_change_schedule->{$value . "_break_time" };
+                        $data['schedule_details'][$value]['start_flexy_time'] =  $drupal_evox_change_schedule->{$value . "_flexy_start" };
+                        $data['schedule_details'][$value]['end_flexy_time'] = $drupal_evox_change_schedule->{$value . "_flexy_end" };
                         }
-
                     }
-
                     $schedule =  $schedule->store($data);
 
                     $change_schedule                 = new ChangeSchedule();
                     $change_schedule->user_id        =  $user->id;
-                    $change_schedule->date           =  $drupal_evox_change_schedule->date;
 
                     $change_schedule->schedule_id    = $schedule->id ;
                     $change_schedule->valid_from     = $drupal_evox_change_schedule->valid_from ;
                     $change_schedule->valid_to       = $drupal_evox_change_schedule->valid_to ;
                     $change_schedule->employee_note  = $drupal_evox_change_schedule->note ;
+                    $change_schedule->status         = $drupal_evox_change_schedule->status;
                     $change_schedule->updated_by     = $user->id;
                     $change_schedule->created_by     = $user->id;
                     
-                    $alter_log->created_at          =  $drupal_evox_change_schedule->date_created;
-                    $alter_log->updated_at          =  $drupal_evox_change_schedule->date_updated;
+                    $change_schedule->created_at          =  $drupal_evox_change_schedule->date_created;
+                    $change_schedule->updated_at          =  $drupal_evox_change_schedule->date_updated;
 
                     
                     $change_schedule->save();
 
                     // Saved the To compute Items
-                    if( in_array($rest_day_work->status, array('approved','declined')) ) {
-                        $to_compute_items[] = $rest_day_work;
+                    if( in_array($change_schedule->status, array('approved','declined')) ) {
+                        $to_compute_items[] = $change_schedule;
                     }
 
-                    log_to_file( 'info', 'Success', [$rest_day_work->getAttributes()], "drupal_migration");
+                    log_to_file( 'info', 'Success', [$change_schedule->getAttributes()], "drupal_migration");
 
                 } else {
-                    log_to_file( 'info', 'User not existing', [$drupal_evox_alter_log], "drupal_migration");
-                    $users_not_existing[$drupal_evox_alter_log->emp_num] = $drupal_evox_alter_log->emp_num;
+                    log_to_file( 'info', 'User not existing', [$drupal_evox_change_schedule], "drupal_migration");
+                    $users_not_existing[$drupal_evox_change_schedule->employee_number] = $drupal_evox_change_schedule->employee_number;
                 }
-
             }
 
             DB::commit();
@@ -286,8 +288,6 @@ class ChangeScheduleRepository implements ChangeScheduleRepositoryInterface{
             log_error($e);
             throw $e;
         }
-
-
 
     }
 
