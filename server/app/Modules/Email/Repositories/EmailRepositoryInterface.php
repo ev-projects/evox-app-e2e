d60e4da6950ca90b3658d04bc31dcd7f110651a1
aaron-colina-ev <aaron.colina@eastvantage.com>
2021-01-19 12:30:51 +0800

    sprint-30_story-393016: BE: Request Rest Day Work Email Notification
    Rest Day Work E-mail Sending:
    - Created a Mailable for the Rest Day Work module to be sent in every new Rest Day Work request being submitted.

--- server/app/Modules/Email/Repositories/EmailRepositoryInterface.php
+++ server/app/Modules/Email/Repositories/EmailRepositoryInterface.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Email\Repositories;
 
 use App\Modules\Request\Models\Overtime;
+use App\Modules\Request\Models\RestDayWork;
 use App\Modules\User\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -11,4 +12,8 @@ interface EmailRepositoryInterface
     public function sendOvertimeRequestEmail( Overtime $overtime );
 
     public function sendOvertimeRequestChangeStatusEmail( Overtime $overtime );
+
+    public function sendRestDayWorkRequestEmail( RestDayWork $rest_day_work );
+
+    public function sendRestDayWorkRequestChangeStatusEmail( RestDayWork $rest_day_work );
 }
