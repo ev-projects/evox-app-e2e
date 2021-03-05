358d4be3453888123e6be1378862f69d84010da8
aaron-colina-ev <aaron.colina@eastvantage.com>
2021-03-05 10:27:13 +0800

    CSS Changes from Ms. Mela

--- client/src/container/Dashboard/Dashboard.js
+++ client/src/container/Dashboard/Dashboard.js
@@ -39,7 +39,7 @@ class Dashboard extends Component {
                             
                                     </div>
                                     <div className="col-lg-12 col-md-12 col-sm-12">
-                                    <h2 align="center">2021 February IT schedule</h2>
+                                    <h2 align="center">IT schedule</h2>
                                     <img src="https://eastvantage.com/evox/ITSched.jpg" width="100%"/>
                                       <ul><li>For IT concerns please send an email to helpdesk@eastvantage.com</li>
                                       <li>To follow up, you may chat us on skype at ev.it.helpdesk</li>
@@ -53,29 +53,28 @@ class Dashboard extends Component {
                             <div className="evbuddy">
 
                         <h3 align="center"><strong>EV</strong> JOB OPENINGS</h3>
-<ul><li><a href="https://careers.eastvantage.com/content/spanish-portuguese-bilingual-specialist-bgc">Spanish - Portuguese Bilingual Specialist | BGC</a></li>
-<li><a href="https://careers.eastvantage.com/content/sr-netsuite-erp-principal-consultant-benelux">Senior NetSuite&nbsp; ERP Principal Consultant | Benelux</a></li>
-<li><a href="https://careers.eastvantage.com/content/us-mortgage-loan-specialist-end-end-process-mandaluyong">US Mortgage Loan Specialist (End to End Process) | Mandaluyong</a></li>
-<li><a href="https://careers.eastvantage.com/content/senior-php-software-developer-backend-bgc">Senior PHP Software Developer (Backend) | BGC</a></li>
-<li><a href="https://careers.eastvantage.com/content/senior-php-software-engineer-laraveljavascript-bgc">PHP Software Engineer (Laravel/Javascript) | BGC</a></li>
-<li><a href="https://careers.eastvantage.com/content/lead-rpa-application-developer-blue-prism-makati-city">Lead RPA Application Developer (Blue Prism) | Makati City</a></li>
-<li><a href="https://careers.eastvantage.com/content/rpa-engineer-blue-prism-makati">Robotics Process Automation Engineer (Blue Prism) | Makati</a></li>
-<li><a href="https://careers.eastvantage.com/content/application-support-analyst">Application Support Analyst (Level 2)</a></li>
-<li><a href="https://careers.eastvantage.com/content/php-full-stack-developer-laravel-reactjs">PHP Full Stack Developer | Laravel | ReactJS</a></li>
-<li><a href="https://careers.eastvantage.com/content/customer-service-head-bgc-taguig">Customer Service Head</a></li>
-<li><a href="https://careers.eastvantage.com/content/technical-support-representative-billinginvoices-non-voice-graveyard" target="_blank">Technical Support Representative (Billing/Invoices)</a></li>
-<li><a href="https://careers.eastvantage.com/content/senior-full-stack-php-developer-laravel-reactjs" target="_blank">Senior Full Stack PHP Developer (Laravel, ReactJS)</a></li>
-<li><a href="https://careers.eastvantage.com/content/full-stack-developer-laravelreactjs-bgc-taguig" target="_blank">Full Stack Developer (Laravel/ReactJS)</a></li>
-<li><a href="https://careers.eastvantage.com/content/senior-full-stack-web-developer-phpjavascript-bgc" target="_blank">Senior Full Stack Web Developer (PHP/Javascript)</a></li>
-<li><a href="https://careers.eastvantage.com/content/customer-service-manager-bgc-taguig" target="_blank">Customer Service Manager | BGC, Taguig</a></li>
-<li><a href="https://careers.eastvantage.com/content/senior-elixir-software-engineer-full-stack-bgc" target="_blank">Senior Elixir Software Engineer (Full Stack)</a></li>
-<li><a href="https://careers.eastvantage.com/content/us-mortgage-loan-closer-mandaluyong"><u>US Mortgage Loan Closer | Night Shift</u></a></li>
-<li><a href="https://careers.eastvantage.com/content/frontend-web-developer-phpjavascript-bgc-0" target="_blank">Frontend Web Developer (PHP/Javascript)</a></li>
-<li><a href="https://careers.eastvantage.com/content/sr-netsuite-erp-consultant-benelux">Sr. NetSuite ERP Consultant (Benelux)</a></li>
-<li><a href="https://careers.eastvantage.com/content/rpa-lead-developer-blue-prism-certified">RPA Lead Developer (Blue Prism Certified)</a></li>
-<li><a href="https://careers.eastvantage.com/content/customer-service-representative-life-insurance-bgc-taguig"><u>Customer Service Representative (Life Insurance) BGC, Taguig</u></a></li>
-<li><a href="https://careers.eastvantage.com/content/native-japanese-bilingual-learning-specialist-bgc-taguig"><u>Native Japanese Bilingual Learning Specialist&nbsp;</u></a></li>
-<li><u><a href="https://careers.eastvantage.com/content/spanish-portuguese-learning-specialist-bgc-taguig">Spanish - Portuguese Learning Specialist</a></u></li>
+<ul>
+<li><a href="https://careers.eastvantage.com/content/us-mortgage-loan-disclosure-specialist-night-shift-mandaluyong">US Mortgage Loan Disclosure Specialist | Night Shift | Mandaluyong</a></li>
+<li><a href="https://careers.eastvantage.com/content/us-mortgage-loan-specialist-end-end-process-mandaluyong">US Mortgage Loan Specialist (End to End Process)</a></li>
+<li><a href="https://careers.eastvantage.com/content/spanish-portuguese-learning-professional-customer-service-specialist">Spanish-Portuguese Learning Professional | Customer Service Specialist</a></li>
+<li><a href="https://careers.eastvantage.com/content/portuguese-learning-specialist-project-based-bgc-taguig">Portuguese Learning Specialist | Project-Based</a></li>
+<li><a href="https://careers.eastvantage.com/content/mandarin-learning-specialist-customer-support-representative">Mandarin Learning Specialist | Customer Support Representative</a></li>
+<li><a href="https://careers.eastvantage.com/content/english-learning-specialist-customer-service-professional">English Learning Specialist | Customer Service Professional</a></li>
+<li><a href="https://careers.eastvantage.com/content/japanese-learning-specialist-customer-care-professional">Japanese Learning Specialist | Customer Care Professional</a></li>
+<li><a href="https://careers.eastvantage.com/content/customer-support-representative-live-chat-email-phone">Customer Support Representative</a></li>
+<li><a href="https://careers.eastvantage.com/content/portuguese-bilingual-specialist-project-based-bgc">Portuguese Bilingual Specialist (Project-Based)</a></li>
+<li><a href="https://careers.eastvantage.com/content/3-5-years-php-backend-laravel-developer-mid-shift">3-5 years | PHP Backend Laravel Developer | Mid Shift</a></li>
+<li><a href="https://careers.eastvantage.com/content/mid-senior-python-developer-3-5-years-bgc-taguig">Mid to Senior Python Developer (3-5 years | BGC, Taguig)</a></li>
+<li><a href="https://careers.eastvantage.com/content/mid-senior-etl-specialist-python">Mid to Senior ETL Specialist (Python)</a></li>
+<li><a href="https://careers.eastvantage.com/content/network-engineer-day-shift-bgc">Network Engineer | Day Shift | BGC</a></li>
+<li><a href="https://careers.eastvantage.com/content/elixir-software-engineer-full-stack-mid-senior-bgc">Senior Elixir Software Engineer (Full Stack)</a></li>
+<li><a href="https://careers.eastvantage.com/content/mandarin-bilingual-specialist-bgc">Mandarin Bilingual Specialist</a></li>
+<li><a href="https://careers.eastvantage.com/content/software-qa-automation-engineer-robot-framework-mid-senior-bgc">Software QA Automation Engineer (Robot Framework)</a></li>
+<li><a href="https://careers.eastvantage.com/content/customer-service-specialist-logisticsfreight-dayshift-bgc">Customer Service Officer (Logistics/Freight)</a></li>
+<li><a href="https://careers.eastvantage.com/content/full-stack-developerreact-js-developer-bgc">Full Stack Developer/React JS Developer</a></li>
+<li><a href="https://careers.eastvantage.com/content/software-engineer-pythonetl-mid-senior-bgc">Software Engineer (Python/ETL)</a></li>
+<li><a href="https://careers.eastvantage.com/content/senior-php-software-developer-backend-bgc">Senior PHP Software Developer (Laravel/Javascript)</a></li>
+<li><a href="https://careers.eastvantage.com/content/drupal-web-developer-php-mid-senior-dayshift">PHP Developer (Drupal)</a></li>
 </ul><h4 align="center"><strong>Refer a friend and earn as much as 20K!</strong></h4>
 <div align="center">Go to <a href="https://careers.eastvantage.com/evbuddy">https://careers.eastvantage.com/evbuddy</a></div>
 </div>
