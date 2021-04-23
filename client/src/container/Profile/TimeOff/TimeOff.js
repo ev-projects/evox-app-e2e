8ddbd6de27ad2eb31d3770b21d6ba64f6734a038
aaron-colina-ev <aaron.colina@eastvantage.com>
2021-04-23 11:02:15 +0800

    Improvement for client-dashboard_sprint-3_story-413056: As user, in leaves page, I can filter Monthly, Weekly, and Daily
    Time-Off
    - Added new tab named "Custom" which allows the user to view their leaves on customized dates.

--- client/src/container/Profile/TimeOff/TimeOff.js
+++ client/src/container/Profile/TimeOff/TimeOff.js
@@ -15,8 +15,9 @@ import Authenticator from "../../../services/Authenticator";
 import { Link } from "react-router-dom"; 
 import moment from 'moment';
 import Formatter from "../../../services/Formatter";
-import RestDayWork from "../../Request/RestDayWork";
+import { InputDate } from '../../../components/DatePickerComponent/DatePicker.js';
 import LeaveCredits from "../LeaveCredits";
+import DatePicker from "react-datepicker";
 
 const TimeOff = ( props ) => {
 
@@ -25,85 +26,104 @@ const TimeOff = ( props ) => {
 
    const [viewType, setViewType] = useState("month");
 
+   const [customStartDate, setCustomStartDate] = useState( new Date() );
+   const [customEndDate, setCustomEndDate] = useState( new Date() );
+
+    // Handles the changing of the View Type 
    const handleChangeViewType = ( type ) => {
-        switch( type ){
+       
+       // If the type is "custom", proceed on handling the Custom Filter function
+       if( type === "custom" ){
+            handleCustomFilter();
+       } else {
+            switch( type ){
 
-            // Change the view type to "day" and set the current start_date as the basis of the start and end date.
-            case "day":
-                start_date.set(moment().startOf('day').toObject());
-                end_date.set(start_date.toObject()).endOf('day');
-                break;
+                // Change the view type to "day" and set the current start_date as the basis of the start and end date.
+                case "day":
+                    start_date.set(moment().startOf('day').toObject());
+                    end_date.set(start_date.toObject()).endOf('day');
+                    break;
 
-            // Change the view type to "week" and set the current start_date as the basis of the start and end date.
-            case "week":
-                start_date.set(moment().startOf('week').toObject());
-                end_date.set(start_date.toObject()).endOf('week');
-                break;
+                // Change the view type to "week" and set the current start_date as the basis of the start and end date.
+                case "week":
+                    start_date.set(moment().startOf('week').toObject());
+                    end_date.set(start_date.toObject()).endOf('week');
+                    break;
 
-            // Change the view type to "month" and set the current start_date as the basis of the start and end date.
-            case "month":
-                start_date.set(moment().startOf('month').toObject());
-                end_date.set(start_date.toObject()).endOf('month');
-                break;
-        }
-        props.fetchTimeOff( profile.details.id, start_date, end_date )
-        setViewType(type);
+                // Change the view type to "month" and set the current start_date as the basis of the start and end date.
+                case "month":
+                    start_date.set(moment().startOf('month').toObject());
+                    end_date.set(start_date.toObject()).endOf('month');
+                    break;
+            }
+            props.fetchTimeOff( profile.details.id, start_date, end_date )
+       }
+       setViewType(type);
     }
 
-   const handleChangeNavigateAction = ( action ) => {
+    // Handles the navigation of view "prev" & "next" depending on the view type selected
+    const handleChangeNavigateAction = ( action ) => {
+ 
+         switch( action ){
+ 
+             // If the action type is "next"
+             case "next":
+                 switch( viewType ){
+ 
+                     // If the viewType is "day", move 1 day forward.
+                     case "day":
+                         start_date.add(1, 'day').startOf('day');
+                         end_date.set(start_date.toObject()).endOf('day');
+                         break;
+ 
+                     // If the viewType is "week", move 1 week forward.
+                     case "week":
+                         start_date.add(1, 'week').startOf('week');
+                         end_date.set(start_date.toObject()).endOf('week');
+                         break;
+ 
+                     // If the viewType is "month", move 1 month forward.
+                     case "month":
+                         start_date.add(1, 'month').startOf('month');
+                         end_date.set(start_date.toObject()).endOf('month');
+                         break;
+                 }
+                 break;
+ 
+             // If the action type is "next"
+             case "prev":
+                 switch( viewType ){
+ 
+                     // If the viewType is "day", move 1 day backward.
+                     case "day":
+                         start_date.subtract(1, 'day').startOf('day');
+                         end_date.set(start_date.toObject()).endOf('day');
+                         break;
+ 
+                     // If the viewType is "week", move 1 week backward.
+                     case "week":
+                         start_date.subtract(1, 'week').startOf('week');
+                         end_date.set(start_date.toObject()).endOf('week');
+                         break;
+ 
+                     // If the viewType is "month", move 1 month backward.
+                     case "month":
+                         start_date.subtract(1, 'month').startOf('month');
+                         end_date.set(start_date.toObject()).endOf('month');
+                         break;
+                 }
+                 break;
+         }
+         props.fetchTimeOff( profile.details.id, start_date, end_date )
+    }
 
-        switch( action ){
-
-            // If the action type is "next"
-            case "next":
-                switch( viewType ){
-
-                    // If the viewType is "day", move 1 day forward.
-                    case "day":
-                        start_date.add(1, 'day').startOf('day');
-                        end_date.set(start_date.toObject()).endOf('day');
-                        break;
-
-                    // If the viewType is "week", move 1 week forward.
-                    case "week":
-                        start_date.add(1, 'week').startOf('week');
-                        end_date.set(start_date.toObject()).endOf('week');
-                        break;
-
-                    // If the viewType is "month", move 1 month forward.
-                    case "month":
-                        start_date.add(1, 'month').startOf('month');
-                        end_date.set(start_date.toObject()).endOf('month');
-                        break;
-                }
-                break;
-
-            // If the action type is "next"
-            case "prev":
-                switch( viewType ){
-
-                    // If the viewType is "day", move 1 day backward.
-                    case "day":
-                        start_date.subtract(1, 'day').startOf('day');
-                        end_date.set(start_date.toObject()).endOf('day');
-                        break;
-
-                    // If the viewType is "week", move 1 week backward.
-                    case "week":
-                        start_date.subtract(1, 'week').startOf('week');
-                        end_date.set(start_date.toObject()).endOf('week');
-                        break;
-
-                    // If the viewType is "month", move 1 month backward.
-                    case "month":
-                        start_date.subtract(1, 'month').startOf('month');
-                        end_date.set(start_date.toObject()).endOf('month');
-                        break;
-                }
-                break;
-        }
+    // Handles the fetching of Customized filter
+    const handleCustomFilter = () => {
+        
+        start_date.set( moment(customStartDate).toObject())
+        end_date.set( moment(customEndDate).toObject())
         props.fetchTimeOff( profile.details.id, start_date, end_date )
-   }
+    }
 
    return ( 
         Validator.isValid( profile ) ?
@@ -127,13 +147,48 @@ const TimeOff = ( props ) => {
                             <Tab eventKey="day" title="Today" type="submit"></Tab>
                             <Tab eventKey="week" title="Weekly" type="submit"></Tab>
                             <Tab eventKey="month" title="Monthly" type="submit"></Tab>
+                            <Tab eventKey="custom" title="Custom" type="submit"></Tab>
                         </Tabs>
-                    </div>
-                    <div className="navigator">
-                        <i className="fa fa-angle-left view-navigate" onClick={(e) => handleChangeNavigateAction("prev")} /> 
-                        <i className="fa fa-angle-right view-navigate"  onClick={(e) => handleChangeNavigateAction("next")} />
-                    </div>
-                    <div className="dates-label">{ Validator.isValid( start_date ) && Validator.isValid( end_date ) ? (start_date.format("LL") === end_date.format("LL") ? start_date.format("LL") : start_date.format("LL") + " - " + end_date.format("LL") )  : null }</div>
+                    </div> 
+                    { viewType == "custom" ? 
+                        <React.Fragment>
+                            <DatePicker 
+                                className="custom-date"                      
+                                showDateSelect
+                                showDateSelectOnly
+                                timeCaption="Time"
+                                dateFormat="MMMM d, yyyy"
+                                timeFormat="MMMM d, yyyy"
+                                placeholder="Start Date"
+                                selected={customStartDate}   
+                                onChange={date => setCustomStartDate(date)} 
+                            />
+                             <span style={{marginTop: "5px"}}>to</span>
+                            <DatePicker 
+                                className="custom-date"                      
+                                showDateSelect
+                                showDateSelectOnly
+                                timeCaption="Time"
+                                dateFormat="MMMM d, yyyy"
+                                timeFormat="MMMM d, yyyy"
+                                placeholder="End Date"
+                                selected={customEndDate}       
+                                onChange={date => setCustomEndDate(date)} 
+                            />                        
+                            <Button className="custom-filter-btn" variant="primary" onClick={handleCustomFilter} >
+                                <i className="fa fa-filter" />
+                            </Button>
+                        </React.Fragment>
+                        :
+                        <React.Fragment>
+                            <div className="navigator">
+                                <i className="fa fa-angle-left view-navigate" onClick={(e) => handleChangeNavigateAction("prev")} /> 
+                                <i className="fa fa-angle-right view-navigate"  onClick={(e) => handleChangeNavigateAction("next")} />
+                            </div>
+                            <div className="dates-label">{ Validator.isValid( start_date ) && Validator.isValid( end_date ) ? (start_date.format("LL") === end_date.format("LL") ? start_date.format("LL") : start_date.format("LL") + " - " + end_date.format("LL") )  : null }</div>
+                        </React.Fragment>
+                    }
+
                 </Row>
             { profile.leaves_list?.length > 0 ? 
                 <div>
@@ -156,7 +211,7 @@ const TimeOff = ( props ) => {
                 }
                 </div>
                 :
-                <div className="no-leaves-row">You don't have any leaves this {viewType}.</div>
+                <div className="no-leaves-row">You don't have any leaves { viewType === "custom" ? "within this date range" : "this " + viewType}.</div>
             } 
             </div>
         </Row>
