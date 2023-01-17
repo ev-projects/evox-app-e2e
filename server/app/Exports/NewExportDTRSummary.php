c21d6f4c19ebd4926a08c0c86b6a84d6dc6a227f
re-gheo <104881716+re-gheo@users.noreply.github.com>
2023-01-17 13:58:01 +0800

    Update NewExportDTRSummary.php

--- server/app/Exports/NewExportDTRSummary.php
+++ server/app/Exports/NewExportDTRSummary.php
@@ -34,11 +34,77 @@ class NewExportDTRSummary implements FromCollection,  WithHeadings
 
         return [
 
-            'sample',
+            "EmployeeName",
 
-            'sample',
-
-            'sample',
+            "emp_num",
+            
+            "email",
+            
+            "username",
+            
+            "ul",
+            
+            "vl_sl",
+            
+            "reg_late",
+            
+            "reg_under_time",
+            
+            "reg_rendered_hr",
+            
+            "reg_night_dif",
+            
+            "reg_over_time",
+            
+            "reg_over_night_dif",
+            
+            "rd_rendered_hr",
+            
+            "rd_night_dif",
+            
+            "rd_over_time",
+            
+            "rd_over_night_dif",
+            
+            "lh_rendered_hr",
+            
+            "lh_night_dif",
+            
+            "lh_over_time",
+            
+            "lh_over_night_dif",
+            
+            "sh_rendered_hr",
+            
+            "sh_night_dif",
+            
+            "sh_over_time",
+            
+            "sh_over_night_dif",
+            
+            "dsh_rendered_hr",
+            
+            "dsh_night_dif",
+            
+            "dsh_over_time",
+            
+            "dsh_over_night_dif",
+            
+            "dlh_rendered_hr",
+            
+            "dlh_night_dif",
+            
+            "dlh_over_time",
+            
+            "dlh_over_night_dif",
+            
+            "slh_rendered_hr",
+            
+            "slh_night_dif",
+            
+            "slh_over_time",
+            
+            "slh_over_night_dif",
 
         ];
 
