af24461f1544b39d4ae9a2149784700e1873af8b
Glenn Macasarte <glenn.macasarte@eastvantage.com>
2023-01-25 11:37:58 +0800

    IT and OPS schedule update

--- client/src/container/OpsSchedule/OpsSchedule.js
+++ client/src/container/OpsSchedule/OpsSchedule.js
@@ -18,7 +18,7 @@ class OpsSchedule extends Component {
 				<div className="col-11 card-body">
 					<div className="h3">IT Department</div>
 						<p>For IT concerns please send an email or chat us via google chat at helpdesk@eastvantage.com</p>
-						<img src="/images/ITSched.jpg" width="100%" />
+						<img src="/images/ITSched.png" width="100%" />
 					
 					
 				</div>
@@ -41,69 +41,72 @@ class OpsSchedule extends Component {
 						</thead>
 						<tbody>
 							<tr>
-								<td rowspan="5">HR</td>
-								<td>Hardy<br/><small>HR Head</small></td>
+								<td rowspan="7">HR</td>
+								<td>Hardy Jacob<br/><small>HR Head</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
-								<td rowspan="5">happiness@eastvantage.com</td>
+								<td rowspan="7">happiness@eastvantage.com</td>
 							</tr>
 							<tr>
+								<td>Precious Delos Reyes<br/><small>HR Manager</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
+							<tr>
+								<td>Justin Loayon<br/><small>HR Service Delivery Officer</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
+							<tr>
+								<td>Jengel Perez<br/><small>HR Generalist</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
+							<tr>
+								<td>Joemark Delima<br/><small>HR Project and Service Delivery Officer</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
+							<tr>
+								<td>Patrick Pineda<br/><small>Learning and Development Lead</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
+							<tr>
+								<td>Yong Mustard<br/><small>Happiness Ambassador</small></td>
+								<td><small>M-F <br/>9am-6pm</small></td>
+							</tr>
 
-								<td>Denver<br/><small>HR Manager</small></td>
-								<td><small>M-F <br/>9am-6pm</small></td>
-
-							</tr>
-							<tr>
-								
-								<td>Jen<br/><small>HR Generalist</small></td>
-								<td><small>M-F <br/>9am-6pm</small></td>
-								
-							</tr>
-							<tr>
-								
-								<td>Justin<br/><small>HR Admin</small></td>
-								<td><small>M-F <br/>9am-6pm</small></td>
-								
-							</tr>
-							<tr>
-								
-								<td>Yong<br/><small>Happiness Ambassador</small></td>
-								<td><small>M-F <br/>9am-6pm</small></td>
-								
-							</tr>
 							<tr>
 								<td rowspan="2">Recruitment</td>
-								<td>Anna<br/><small>Recruitment Head</small></td>
+								<td>Precious Balbedina<br/><small>CRM Recruitment Lead</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
 								<td rowspan="2">recruit@eastvantage.com</td>
 							</tr>
 							<tr>
-								<td>Marian<br/><small>Recruitment Supervisor</small></td>
+								<td>MJ Visitacion<br/><small>Tech Recruitment Lead</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
 							</tr>
-							
+
 							<tr>
 								<td>Procurement</td>
-								<td>Ivan<br/><small>Senior Procurement Officer</small></td>
+								<td>Ivan Batle<br/><small>Senior Procurement Officer</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
 								<td>procurement@eastvantage.com</td>
 							</tr>
+
 							<tr>
 								<td rowspan="3">Facilities</td>
-								<td>Ivan<br/><small>Senior Procurement Officer</small></td>
+								<td>Ivan Batle<br/><small>Senior Procurement Officer</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
 								<td rowspan="3">facilities@eastvantage.com</td>
 							</tr>
 							<tr>
-								<td>Jonathan<br/><small>Facilities/Messenger</small></td>
+								<td>Jonathan Dogillo<br/><small>Facilities/Messenger</small></td>
 								<td><small>M-F <br/>8am-5pm</small></td>
 							</tr>
 							<tr>
-								<td>Wilma<br/><small>Facilities</small></td>
+								<td>Wilma Español<br/><small>Facilities</small></td>
 								<td><small>M-F <br/>7am-4pm</small></td>
 							</tr>
+
 							<tr>
 								<td>Projects<br/><small>(EVOX & NEO)</small></td>
-								<td>Carmela<br/><small>Project Manager</small></td>
+								<td>Vishnu Padmanabhan<br/><small>Global Software Lead</small></td>
 								<td><small>M-F <br/>9am-6pm</small></td>
 								<td>evox@eastvantage.com</td>
 							</tr>
