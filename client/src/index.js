5115413e0f8ba3f34954222a3b8e2c9ad8817401
garyaure-ev <gary.aure@eastvantage.com>
2023-05-24 17:51:22 +0800

    Added axios interceptor for duplicate API calls

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,57 @@ import "./config/GlobalVariables";
 // Imports the store component
 import store from './store'
 
+import axios from "axios";
 
+// abort duplicate request
+const pending = {}
+const CancelToken = axios.CancelToken
+const removePending = (config, f) => {
+  // make sure the url is same for both request and response
+  const url = config.url.replace(config.baseURL, '/')
+  // stringify whole RESTful request with URL params
+  const flagUrl = url + '&' 
+                  + config.method + '&' 
+                  + JSON.stringify(config.params)
+  if (flagUrl in pending) {
+    if (f) {
+      f() // abort the request
+    } else {
+      delete pending[flagUrl]
+    }
+  } else {
+    if (f) {
+      pending[flagUrl] = f // store the cancel function
+    }
+  }
+}
+// axios interceptors
+axios.interceptors.request.use(config => {
+  // you can apply cancel token to all or specific requests
+  // e.g. except config.method == 'options'
+  config.cancelToken = new CancelToken((c) => {
+    removePending(config, c)
+  })
+  return config
+}, error => {
+  Promise.reject(error)
+})
+axios.interceptors.response.use(
+  response => {
+    removePending(response.config)
+    return response
+  },
+  error => {
+    removePending(error.config)
+    
+    if (!axios.isCancel(error)) {
+      return Promise.reject(error)
+    } else {
+      // return empty object for aborted request
+      return Promise.resolve({})
+    }
+  }
+)
 
 ReactDOM.render(
   <BrowserRouter>
