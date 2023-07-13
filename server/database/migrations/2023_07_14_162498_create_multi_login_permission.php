6540d81fb684566ff9238e5ca2316cc74e0927cd
re-gheo <104881716+re-gheo@users.noreply.github.com>
2023-07-13 14:14:59 +0800

    Applied Per-user multi Login

---
+++ server/database/migrations/2023_07_14_162498_create_multi_login_permission.php
@@ -0,0 +1,46 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMultiLoginPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $employee_role = Role::where('name', 'employee')->first();
+        $employee_permission = [
+            Permission::create(['name' => 'user_multi_login', 'label' => 'Use Multi-Login']),
+        ];
+        $employee_role->givePermissionTo($employee_permission);
+
+
+        
+        $supervisor_role = Role::where('name', 'supervisor')->first();
+        $supervisor_permission = [
+            Permission::create(['name' => 'edit_multi_login', 'label' => 'Edit Multi-Login']),
+        ];
+        $supervisor_role->givePermissionTo($supervisor_permission);
+
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('users', function (Blueprint $table) {
+        //     //
+        // });
+    }
+}
