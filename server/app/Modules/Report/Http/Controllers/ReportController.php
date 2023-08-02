b887ace443e912963676cddac23e80aa8ff27f16
garyaure-ev <gary.aure@eastvantage.com>
2023-08-02 21:04:07 +0800

    Make sure country strict is for HR only

--- server/app/Modules/Report/Http/Controllers/ReportController.php
+++ server/app/Modules/Report/Http/Controllers/ReportController.php
@@ -641,7 +641,7 @@ class ReportController extends Controller
             $new_end_date = Carbon::parse($end_date)->format('y-m-d');
             $period = CarbonPeriod::between($new_start_date,  $new_end_date);
 
-            $user_collection = $this->user->get_users_under_supervisee($request, $start_date, $end_date,  true, true);
+            $user_collection = $this->user->get_users_under_supervisee($request, $start_date, $end_date,  true, auth()->user()->hasRole( get_constant('USER_ROLES.hr') ));
 
 
             $override = $this->report->get_team_attendance_summary($user_collection,  $start_date, $end_date);
@@ -691,7 +691,7 @@ class ReportController extends Controller
         $period = CarbonPeriod::between($new_start_date,  $new_end_date);
 
 
-        $user_collection = $this->user->get_users_under_supervisee($request, $start_date, $end_date, true, true);
+        $user_collection = $this->user->get_users_under_supervisee($request, $start_date, $end_date, true, auth()->user()->hasRole( get_constant('USER_ROLES.hr') ));
         $data =  $this->report->get_team_attendance_summary_dtr($user_collection,  $start_date, $end_date);
 
         $list = (array) $data['employee_list_summary'];
