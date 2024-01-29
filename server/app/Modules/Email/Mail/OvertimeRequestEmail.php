71b17a47277097965e5bb4c83a0c61d9d794682a
re-gheo <104881716+re-gheo@users.noreply.github.com>
2024-01-29 13:28:31 +0800

    Revert Overtime request

--- server/app/Modules/Email/Mail/OvertimeRequestEmail.php
+++ server/app/Modules/Email/Mail/OvertimeRequestEmail.php
@@ -2,14 +2,13 @@
 
 namespace App\Modules\Email\Mail;
 
-use Exception;
+use App\Modules\Request\Models\Overtime;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use App\Modules\User\Models\User;
-use Illuminate\Support\Facades\App;
 use Illuminate\Queue\SerializesModels;
-use App\Modules\Request\Models\Overtime;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\App;
+use App\Modules\User\Models\User;
 
 class OvertimeRequestEmail extends Mailable
 {
@@ -28,30 +27,21 @@ class OvertimeRequestEmail extends Mailable
      */
     public function __construct( User $recepient, Overtime $overtime )
     {   
-      try {
-          # Declare the variables to be used for this Email
+        # Declare the variables to be used for this Email
+        $this->recepient     = $recepient;
+        $this->overtime      = $overtime;
+        $this->user          = $overtime->user()->first();
+        $this->department    = $this->user->department()->first();
+        $this->approval_link = env('FRONT_END_URL') . 'request/approval/'.parse_request_to_hash_code( $this->overtime, $this->recepient );
 
-          $this->recepient     = $recepient;
-          $this->overtime      = $overtime;
-          $this->user          = $overtime->user()->first();
-          $this->department    = $this->user->department()->first();
-          $this->approval_link = env('FRONT_END_URL') . 'request/approval/'.parse_request_to_hash_code( $this->overtime, $this->recepient );
-  
-          # If the App is on Production, send on the actual recepient email
-          if( App::environment('production') ) {
-              $this->to( $recepient->email );
-              
-          # If the App is NOT in Production, send on the email on Eastvantage Dev Eamil
-          } else {
-              $this->to( get_constant('EASTVANTAGE_DEV_EMAIL') );
-          }
-          
-           } catch (Exception $e) {
-            error_log($e->getMessage());
-            // log_error($e, 'emails');
-            throw $e;
+        # If the App is on Production, send on the actual recepient email
+        if( App::environment('production') ) {
+            $this->to( $recepient->email );
+            
+        # If the App is NOT in Production, send on the email on Eastvantage Dev Eamil
+        } else {
+            $this->to( get_constant('EASTVANTAGE_DEV_EMAIL') );
         }
-      
     }
 
     /**
