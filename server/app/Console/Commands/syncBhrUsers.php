9c2e430bf427d4e98bc92be15861426015bec0b9
garyaure-ev <gary.aure@eastvantage.com>
2024-06-07 23:07:45 +0800

    Fixed sync issue on invalid birth date value

--- server/app/Console/Commands/syncBhrUsers.php
+++ server/app/Console/Commands/syncBhrUsers.php
@@ -70,14 +70,14 @@ class syncBhrUsers extends Command
             print_r(["Yesterday", $dt_since->toDateTimeString()]);
             $dt_res = call_sp("EH_SP_Bhr_To_Evox_Sync_Logs", [null, null, null, 2]);
             if ((count($dt_res) > 0) and (count($dt_res[0]) > 0)) {
-                print_r(["Last Date Res", $dt_res[0][0]]);
+                #print_r(["Last Date Res", $dt_res[0][0]]);
                 if (!empty($dt_res[0][0]->UDV_Sync_Date)) {
                     $dt_since = Carbon::createFromFormat("Y-m-d H:i:s", $dt_res[0][0]->UDV_Sync_Date);
                     print_r(["Last Date Synced", $dt_since->toDateTimeString()]);
                 }
             }
             $since_date_to_sync = $dt_since->toAtomString();
-            print_r(["Last Timestamp Synced", $since_date_to_sync]);
+            #print_r(["Last Timestamp Synced", $since_date_to_sync]);
             /*if (Cache::has('user_since_date_sync_ts')) {
                 $user_since_date_sync_ts = Cache::get('user_since_date_sync_ts');
                 $dt_since = Carbon::createFromTimestamp($user_since_date_sync_ts);
@@ -101,23 +101,23 @@ class syncBhrUsers extends Command
             } else {
                 $bhr_user_number_array = $this->bhr->get_changed_users($since_date_to_sync);
             }
-            print_r(["BHR NUMBERS", $bhr_user_number_array]);
+            #print_r(["BHR NUMBERS", $bhr_user_number_array]);
             #Fetched last updated BHR Numbers
             #log_to_file('info', "BHR NUM", $bhr_user_number_array, "sync_bhr_user");
 
             $failed_bhr_num =  call_sp("EH_SP_User_Logs", [0, null])[0];
-            print_r(["FAILED BHR NUMBERS", $failed_bhr_num]);
+            #print_r(["FAILED BHR NUMBERS", $failed_bhr_num]);
             foreach ($failed_bhr_num as $fbhr_num) {
                 $bhr_user_number_array["$fbhr_num->bhr_num"] = "$fbhr_num->bhr_num";
             }
-            print_r(["MERGED BHR NUMBERS", $bhr_user_number_array]);
+            #print_r(["MERGED BHR NUMBERS", $bhr_user_number_array]);
             #Merged with failed BHR Number
             #log_to_file('info', "Merged BHR NUM", $bhr_user_number_array, "sync_bhr_user");
 
             # 2.
             # Iterate the actual BHR User Numbers array
             foreach ($bhr_user_number_array as $bhr_user_number) {
-
+                print_r(["To Be Synced", $bhr_user_number]);
                 try {
 
                     // Fetch the User if it's already existing in the System
@@ -126,14 +126,35 @@ class syncBhrUsers extends Command
                     # Fetch the BHr User Details
                     $bhr_user = $this->bhr->get_user($bhr_user_number, true);
                     if (is_valid($bhr_user)) {
-
+                        #print_r(["Fetched User", $bhr_user]);
                         $bhr_status = $bhr_user->status == "Active" ? 1 : 0;
                         $termination_date = $bhr_user->terminationDate == "0000-00-00" ? null : $bhr_user->terminationDate;
                         $SUP_id = $bhr_user->supervisorEId == null ? null : $bhr_user->supervisorEId;
-
                         // error_log($bhr_user);
                         // dd($bhr_user);
-
+                        /*print_r(["Call Param", [
+                            $bhr_user->bestEmail,
+                            $bhr_user->employeeNumber,
+                            $bhr_user->id,
+                            generate_username($bhr_user),
+                            Hash::make(get_constant('DEFAULT_PASSWORD')),
+                            $bhr_user->firstName,
+                            $bhr_user->middleName,
+                            $bhr_user->lastName,
+                            $bhr_user->nickname,
+                            $bhr_user->employmentHistoryStatus,
+                            $bhr_user->hireDate,
+                            $bhr_status,
+                            $bhr_user->jobTitle,
+                            $bhr_user->country,
+                            $bhr_user->dateOfBirth,
+                            $termination_date,
+                            $bhr_user->department,
+                            $bhr_user->mobilePhone,
+                            $SUP_id,
+                            $bhr_user->division,
+                            (new Carbon($bhr_user->lastChanged))->toDateTimeString()
+                        ]]);*/
                         $result =  call_sp(
                             "EH_SP_User_sync",
                             [
@@ -151,7 +172,7 @@ class syncBhrUsers extends Command
                                 $bhr_status,
                                 $bhr_user->jobTitle,
                                 $bhr_user->country,
-                                $bhr_user->dateOfBirth,
+                                $bhr_user->dateOfBirth == "0000-00-00" ? null : $bhr_user->dateOfBirth,
                                 $termination_date,
                                 $bhr_user->department,
                                 $bhr_user->mobilePhone,
@@ -160,7 +181,8 @@ class syncBhrUsers extends Command
                                 (new Carbon($bhr_user->lastChanged))->toDateTimeString()
                             ]
                         );
-
+                        #print_r(["User Res", $result]);
+                        print_r(["User Synced", $bhr_user_number]);
 
                         // $result = DB::select('call EH_SP_User_sync("'.$bhr_user->bestEmail.'", '.$bhr_user->employeeNumber.'
                         //     ,'.$bhr_user->id.', "'.generate_username( $bhr_user ).'","'.Hash::make( get_constant('DEFAULT_PASSWORD') ).'"
@@ -171,7 +193,7 @@ class syncBhrUsers extends Command
 
                         $dt_current = (new Carbon($bhr_user->lastChanged));
                         $res_current = call_sp("EH_SP_Bhr_To_Evox_Sync_Logs", [$bhr_user_number, $dt_current->toDateTimeString(), 1, 1]);
-                        print_r(["Current Synced", $res_current]);
+                        #print_r(["Current Synced", $res_current]);
                         print_r(["Current Date Synced", $dt_current->toDateTimeString()]);
                         print_r(["Current Timestamp Synced", $dt_current->getTimestamp()]);
                         /*$new_timestamp = (new Carbon($bhr_user->lastChanged))->getTimestamp();
@@ -181,6 +203,12 @@ class syncBhrUsers extends Command
                             log_to_file('info', "NEW START TIME", [$user_since_date_sync_ts,  __FUNCTION__], "sync_bhr_user");
                         }*/
                         log_to_file('info', "SYNC SUCCESS", [$bhr_user_number, $bhr_user->lastChanged,  __FUNCTION__], "sync_bhr_user");
+
+                        print_r("\n");
+                        print_r("\n");
+                        print_r("END OF ITEM");
+                        print_r("\n");
+                        print_r("\n");
                     }
                 } catch (Exception $e) {
                     log_to_file('info', 'SYNC ERROR' . [$bhr_user_number, $e, __FUNCTION__], "sync_bhr_user");
