f77c74d8c902e5746e6bc9db79f72ff8c49df3fb
garyaure-ev <gary.aure@eastvantage.com>
2025-02-26 14:21:28 +0800

    Added error handler for fetching redis cache data

--- server/app/Http/Controllers/RedisController.php
+++ server/app/Http/Controllers/RedisController.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Http\Controllers;
+
+use Exception;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Ixudra\Curl\Facades\Curl;
 
@@ -7,26 +10,34 @@ class RedisController extends Controller
 {
     public function get_redis_notifications($user_id)
     {
-        $api_endpoint = '/api/cache/' . $user_id;
-        $response = Curl::to( env('REDIS_SERVER_HOST') . $api_endpoint )
-                            ->withHeader('Accept: application/json')
-                            ->withTimeout(300)
-                            ->withConnectTimeout(300)
-                            ->returnResponseObject()
-                            ->get();
-
-        if ($response->status == 200) {
-            return $response->content;
-        } else if ($response->status == 404) {
-            $content = [
-                'requestsForApproval' => [],
-                'requestStatus' => [],
-                'announcements' => [],
-                'celebrations' => [],
-                'missedDtr' => []
-            ];
-
-            return json_encode($content);
+        $default_content = [
+            'requestsForApproval' => [],
+            'requestStatus' => [],
+            'announcements' => [],
+            'celebrations' => [],
+            'missedDtr' => []
+        ];
+        //return response()->json($default_content);
+        try {
+            $api_endpoint = '/api/cache/' . $user_id;
+            $response = Curl::to( env('REDIS_SERVER_HOST') . $api_endpoint )
+                                ->withHeader('Accept: application/json')
+                                ->withTimeout(300)
+                                ->withConnectTimeout(300)
+                                ->returnResponseObject()
+                                ->get();
+            if ($response->status == 200) {
+                if ((!isset($response->content)) or (empty($response->content))) {
+                    return response()->json($default_content);
+                }
+                return response()->json($response->content);
+            } else if ($response->status == 404) {
+                return response()->json($default_content);
+            } else {
+                return response()->json($default_content);
+            }
+        } catch (Exception $e) {
+            return response()->json($default_content);
         }
     }
 }
