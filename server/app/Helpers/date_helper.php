c585a73dca4f2d9036b756dbdeaf9608033c9a2a
Glenn Macasarte <glenn.macasarte@eastvantage.com>
2025-04-16 19:15:03 +0800

    slight changes in users pov

--- server/app/Helpers/date_helper.php
+++ server/app/Helpers/date_helper.php
@@ -253,7 +253,8 @@ if (! function_exists('timestamp_to_datetime')) {
             if($use_OWNER == true && $owner != null ){
                 $user = $owner;
                 
-                $target_date_offset =  string_offset_to_seconds(Carbon::createFromTimestamp( $timestamp)->setTimezone($user->country_timezone_name())->format("P"));
+                // $target_date_offset =  string_offset_to_seconds(Carbon::createFromTimestamp( $timestamp)->setTimezone($user->country_timezone_name())->format("P"));
+                $target_date_offset = 0;
 
                 if(!(string_offset_to_seconds($user->country_timezone_to_offset()) == ($target_date_offset))){
                     return ( is_valid( $timestamp ) ) ? date('Y-m-d H:i:s', $timestamp+ string_offset_to_seconds($target_date_offset)) : null;
