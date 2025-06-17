5f101c56288cce0d39c587e430288692ce192e84
Glenn Macasarte <glenn.macasarte@eastvantage.com>
2025-06-17 11:02:15 +0800

    fixed nho data values before inserting to table

--- server/app/Http/Controllers/NewHireOrientationController.php
+++ server/app/Http/Controllers/NewHireOrientationController.php
@@ -27,8 +27,30 @@ class NewHireOrientationController extends Controller
     public function store(Request $request)
     {
         try {
-            $request->user_id = auth()->user()->id;
-            $nho = NhoSurvey::insert($request->all() + ['user_id' => Auth::user()->id, 'created_at' => Carbon::now()]);
+            $fields = [
+                'nho_date',
+                'onboarding_exp_rating',
+                'recruitment_exp_rating',
+                'schedule_awareness_rating',
+                'topic_relevance_rating',
+                'facilitator_id',
+                'facilitator_knowledge_rating',
+                'facilitator_presentation_rating',
+                'facilitator_response_rating',
+                'equipment_rating',
+                'accessibility_rating',
+                'welcome_rating',
+                'suggestions',
+                'nho_overall_feedback',
+            ];
+
+            foreach ($fields as $field) {
+                $data[$field] = $request->$field;
+            }
+            $data['user_id'] = Auth::user()->id;
+            $data['created_at'] = Carbon::now();
+
+            $nho = NhoSurvey::insert($data);
 
             if ($nho == 1) {
                 return response()->json(['message' => 'Thank you for completing the NHO Survey! Your response has been successfully submitted.', 'status' => 200], 200);
