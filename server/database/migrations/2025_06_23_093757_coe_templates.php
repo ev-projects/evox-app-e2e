de83750435b7f6b18b9f966170dd25b9bf773e43
garyaure-ev <gary.aure@eastvantage.com>
2025-06-26 14:46:07 +0800

    Fixed migration issue

--- server/database/migrations/2025_06_23_093757_coe_templates.php
+++ server/database/migrations/2025_06_23_093757_coe_templates.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CoeTemplates extends Migration
+class CoeTemplatesMigration extends Migration
 {
     /**
      * Run the migrations.
