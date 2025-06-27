1385bee6b4010565081a3aad10f1da74947e453b
garyaure-ev <gary.aure@eastvantage.com>
2025-06-27 12:26:59 +0800

    Revert "Fixed COE Template Migration"
    This reverts commit f1a2bf4df2794502a50df4178f543338b76b9648.

--- server/database/migrations/2025_06_24_121843_coe_nullable_salary_fields.php
+++ server/database/migrations/2025_06_24_121843_coe_nullable_salary_fields.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CoeNullableSalaryFieldsMigration extends Migration
+class CoeNullableSalaryFields extends Migration
 {
     /**
      * Run the migrations.
