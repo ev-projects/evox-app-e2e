d3f1110be8374c5d642e413191a5748fa9ddeb15
garyaure-ev <gary.aure@eastvantage.com>
2026-05-20 22:18:31 +0800

    Removed insert_users endpoint

--- server/routes/api.php
+++ server/routes/api.php
@@ -39,7 +39,6 @@ Route::get('Getroomlistlocation_wise/{roomid}', 'RoomController@Getroomlistlocat
 Route::get('Gettodayleaves', 'BookingController@get_today_leave_list')->middleware('jwtauth', 'auth.apikey');
 Route::get('Gettommorowleaves', 'BookingController@get_tommorow_leave_list')->middleware('jwtauth', 'auth.apikey');
 Route::get('Getitrequirement', 'BookingController@get_itrequirement_roomlist')->middleware('jwtauth', 'auth.apikey');
-Route::post('insert_users', 'BookingController@insert_user_details');
 Route::post('sync_users', 'SyncController@syncusers')->middleware('auth.apikey');
 Route::post('sync_users_hris', 'SyncController@syncusers_HRIS')->middleware('auth.apikey');
 Route::post('sync_holidays', 'SyncController@syncholidays')->middleware('auth.apikey');
